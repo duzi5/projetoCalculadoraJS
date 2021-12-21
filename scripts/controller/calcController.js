21dcc7416cfc9585b0617383b169d1c146e7320a
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2021-12-21 19:01:47 -0300

    avançando com o controller

---
+++ scripts/controller/calcController.js
@@ -0,0 +1,45 @@
+class CalcController {
+
+
+    constructor(){
+        this._displayCalc; 
+        this._currentDate;
+        this.inicialize();
+    }
+
+    inicialize(){
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+    
+        displayCalcEl.innerHTML = "4567";
+    }
+
+
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+        
+    }   
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+        this._currentDate = valor;
+    }
+
+
+
+
+
+
+
+
+}
