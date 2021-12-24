864e7c38807174c1a64d53bff5d9300be18bba74
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2021-12-23 21:08:00 -0300

    adicionando eventos aos botões

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,45 +1,118 @@
 class CalcController {
 
-
     constructor(){
-        this._displayCalc; 
+
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
-        this.inicialize();
+        this.initialize();
+        this.initButtonsEvents();
+
     }
 
-    inicialize(){
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+    initialize(){
 
+        this.setDisplayDateTime()
+
+        setInterval(()=>{
+
+            this.setDisplayDateTime();
+
+        }, 1000);
+
+    }
+
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        })
     
-        displayCalcEl.innerHTML = "4567";
     }
 
+    initButtonsEvents(){
 
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+
+            this.addEventListenerAll(btn, "click drag", e => {
+
+                console.log(btn.className.baseVal.replace("btn-",""));
+
+            })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
+
+            })
+
+        })
+
+    }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
+    get displayTime(){
+
+        return this._timeEl.innerHTML;
+
+    }
+
+    set displayTime(value){
+
+        return this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate(){
+
+        return this._dateEl.innerHTML;
+
+    }
+
+    set displayDate(value){
+
+        return this._dateEl.innerHTML = value;
+
+    }
 
     get displayCalc(){
-        return this._displayCalc;
+
+        return this._displayCalcEl.innerHTML;
+
     }
 
-    set displayCalc(valor){
-        this._displayCalc = valor;
-        
-    }   
+    set displayCalc(value){
+
+        this._displayCalcEl.innerHTML = value;
+
+    }
 
     get currentDate(){
-        return this._currentDate;
+
+        return new Date();
+
     }
 
-    set currentDate(valor){
-        this._currentDate = valor;
+    set currentDate(value){
+
+        this._currentDate = value;
+
     }
-
-
-
-    //pesquisando sobre o método inicialize
-
-
-
 
 }
