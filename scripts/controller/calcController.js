b5280b602b031f838d786600639d850027c989e6
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2021-12-24 20:41:11 -0300

    evoluindo com as funcionalidades

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,7 +1,7 @@
 class CalcController {
 
     constructor(){
-
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -34,15 +34,86 @@ class CalcController {
     
     }
 
+    clearAll(){
+        this._operation = [];
+    }
+
+    cancelEntry(){
+        this._operation.pop();
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+        console.log(this._operation)
+    }
+
+
+    execBtn(value){
+        switch (value){ 
+            case 'ac':this.clearAll();
+                break;
+            case 'ce':this.cancelEntry();
+                break;
+            case 'soma': 
+                
+                break;
+            case 'subtracao': 
+                
+                break;
+            case 'multiplicacao': 
+                
+                break;
+            case 'divisao': 
+                
+                break;
+            case 'porcento': 
+                
+                break;
+            case 'igual':   
+                break
+            case '0': ;
+            case '1': ;
+            case '2': ;
+            case '3': ;
+            case '4': ;
+            case '5': ;
+            case '6': ;
+            case '7': ;
+            case '8': ;
+            case '9': ;
+                this.addOperation(parseInt(value))
+                break
+
+
+        
+        }
+         
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index)=>{
 
-            this.addEventListenerAll(btn, "click drag", e => {
+            this.addEventListenerAll(btn, "click", e => {
 
-                console.log(btn.className.baseVal.replace("btn-",""));
+              let juiceButton = btn.className.baseVal.replace("btn-","");
+              this.execBtn(juiceButton);
+                
+
+                
+               
 
             })
 
@@ -55,6 +126,8 @@ class CalcController {
         })
 
     }
+
+
 
     setDisplayDateTime(){
 
@@ -82,7 +155,7 @@ class CalcController {
     get displayDate(){
 
         return this._dateEl.innerHTML;
-
+  
     }
 
     set displayDate(value){
