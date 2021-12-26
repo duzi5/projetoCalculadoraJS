9c5107ba0bace1b7403958ed6f1cc6e862dc35a2
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2021-12-26 20:18:35 -0300

    display funcionando

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -84,18 +84,25 @@ class CalcController {
         let result = eval(this._operation.join(""));
 
         this._operation = [result, last];
+        this.setLastNumberToDisplay();
 
     }
 
     setLastNumberToDisplay(){
 
-        
+        let lastNumber; 
 
+        for(let i = this._operation.length-1; i>=0; i--){
+            if(!this.isOperator(this._operation[i])){
+                lastNumber=this._operation[i]
+                break
+            }
+        }
+        this.displayCalc = lastNumber;
     }
 
     addOperation(value){
-
-
+      
         if (isNaN(this.getLastOperation())) {
 
             if (this.isOperator(value)) {
@@ -109,7 +116,7 @@ class CalcController {
             } else {
 
                 this.pushOperation(value);
-
+                this.setLastNumberToDisplay();
             }
 
         } else {
