e92e97b75c21f09c8374c9ce320924d5b1c96415
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2021-12-26 19:48:05 -0300

    limtando o tamanho do vetor, usando os métodos nativos join e eval

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,7 @@
 class CalcController {
 
     constructor(){
+
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -35,127 +36,180 @@ class CalcController {
     }
 
     clearAll(){
+
         this._operation = [];
+
     }
 
-    cancelEntry(){
+    clearEntry(){
+
         this._operation.pop();
+
     }
 
     getLastOperation(){
+
         return this._operation[this._operation.length-1];
+
     }
-    
+
     setLastOperation(value){
-        this._operation[this._operation.length -1] = value;
+
+        this._operation[this._operation.length-1] = value;
+
     }
 
+    isOperator(value){
 
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
 
-   isOperator(value){
-        return(['+', '-', '/', '*', 'porcento'].indexOf > -1); 
-   }
-   
-   
-    addOperation(value){
-        
-        if (isNaN(this.getLastOperation())){
-            
-            if(this.isOperator()){
-                this._operation[this._operation.length -1] = value
-                
-            }else if (isNaN(value)){ 
-                //outra coisa
-                console.log(value)
-            }else{
-                this._operation.push(value);
-            }
-            
+    }
 
-        }else{
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
+    pushOperation(value){
+
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+            this.calc();
+
         }
-        console.log(this._operation);
-        
-       
-        
+
     }
 
+    calc(){
 
+        let last = this._operation.pop();
+        
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+    }
+
+    setLastNumberToDisplay(){
+
+        
+
+    }
+
+    addOperation(value){
+
+
+        if (isNaN(this.getLastOperation())) {
+
+            if (this.isOperator(value)) {
+
+                this.setLastOperation(value);
+
+            } else if (isNaN(value)){
+
+                console.log("outra coisa",value);
+
+            } else {
+
+                this.pushOperation(value);
+
+            }
+
+        } else {
+
+            if (this.isOperator(value)){
+
+                this.pushOperation(value);
+
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+
+            }
+
+        }
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+        
+    }
 
     execBtn(value){
-        switch (value){ 
-            case 'ac':this.clearAll();
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
                 break;
-            case 'ce':this.cancelEntry();
+
+            case 'ce':
+                this.clearEntry();
                 break;
-            case 'soma': 
+
+            case 'soma':
                 this.addOperation('+');
                 break;
-            case 'subtracao': 
+
+            case 'subtracao':
                 this.addOperation('-');
                 break;
-            case 'multiplicacao': 
-                this.addOperation('*');
-                break;
-            case 'divisao': 
+
+            case 'divisao':
                 this.addOperation('/');
                 break;
-            case 'porcento': 
+
+            case 'multiplicacao':
+                this.addOperation('*');
+                break;
+
+            case 'porcento':
                 this.addOperation('%');
                 break;
+
             case 'igual':
-
-                break
-
-            case '.': 
-            this.addOperation('.');
-                break;
                 
-            case '0': ;
-            case '1': ;
-            case '2': ;
-            case '3': ;
-            case '4': ;
-            case '5': ;
-            case '6': ;
-            case '7': ;
-            case '8': ;
-            case '9': ;
-                this.addOperation(parseInt(value))
-                break
+                break;
 
+            case 'ponto':
+                this.addOperation('.');
+                break;
 
-        
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+
         }
-         
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index)=>{
 
-            this.addEventListenerAll(btn, "click", e => {
+            this.addEventListenerAll(btn, "click drag", e => {
 
-              let juiceButton = btn.className.baseVal.replace("btn-","");
-              this.execBtn(juiceButton);
-                
+                let textBtn = btn.className.baseVal.replace("btn-","");
 
-                
-               
+                this.execBtn(textBtn);
 
             })
 
@@ -168,8 +222,6 @@ class CalcController {
         })
 
     }
-
-
 
     setDisplayDateTime(){
 
@@ -197,7 +249,7 @@ class CalcController {
     get displayDate(){
 
         return this._dateEl.innerHTML;
-  
+
     }
 
     set displayDate(value){
